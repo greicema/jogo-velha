f224688bd74caf340720d5eea1c38b010159f3ca
Greice Mara <sgreicemaraj@gmail.com>
2021-03-28 00:11:29 -0300

    checagem do vencedor

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
- var jogador
+ var jogador = null;
+ var jogadorSelecionado = document.getElementById('jogador-selecionado');
+ var quadrados = document.getElementsByClassName('quadrado');
 
+ mudarJogador('X');
 
  function escolherQuadrado(id) {
-     console.log("click");
+     var quadrado = document.getElementById(id);
+
+     if (quadrado.innerHTML !== '-') {
+         return;
+     }
+
+     quadrado.innerHTML = jogador;
+     quadrado.style.color = '#000';
+
+     if (jogador === 'X') {
+         jogador = 'O';
+     } else {
+         jogador = 'X';
+     }
+
+     mudarJogador(jogador);
+ }
+
+ function mudarJogador(valor) {
+     jogador = valor;
+     jogadorSelecionado.innerHTML = jogador;
+ }
+
+ function checaVencedor() {
+     var quadrado1 = document.getElementById(1);
+     var quadrado2 = document.getElementById(2);
+     var quadrado3 = document.getElementById(3);
+     var quadrado4 = document.getElementById(4);
+     var quadrado5 = document.getElementById(5);
+     var quadrado6 = document.getElementById(6);
+     var quadrado7 = document.getElementById(7);
+     var quadrado8 = document.getElementById(8);
+     var quadrado9 = document.getElementById(9);
  }
